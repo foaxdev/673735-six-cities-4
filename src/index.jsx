025892e4667c943b4f770a9c2a19ae1183520af4
Irina Kovalchuk <foaxdev@yahoo.com>
2020-06-14 14:12:14 +0300

    Add propTypes

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import {App} from './components/App/App';
 
 const AVAILABLE_OFFERS = 10;
+const OFFERS_TITLES = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`];
 
 ReactDOM.render(
-    <App availableOffers={AVAILABLE_OFFERS} />,
+    <App availableOffers={AVAILABLE_OFFERS} offers={OFFERS_TITLES} />,
     document.querySelector(`#root`)
 );
